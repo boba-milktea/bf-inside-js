5f0ee44a4085a57efede06a87bae457c75719a5f
boba-milktea <catherine.idv@gmail.com>
2025-03-08 13:40:20 +0100

    add exercises3

--- 2-control-flow/1-isolate/2-refactoring-truthiness-operators/exercises/condition-to-and.js
+++ 2-control-flow/1-isolate/2-refactoring-truthiness-operators/exercises/condition-to-and.js
@@ -7,14 +7,17 @@ console.log('-- begin --');
 // refactor this code to use && instead of a conditional
 
 // try different values and different types
-const input = _;
+const input = 'pickles';
 const password = 'pickles';
 console.log(input, password);
 
 let isValidPassword = typeof input === 'string';
-if (isValidPassword) {
-  isValidPassword = input === password;
-}
+
+isValidPassword ?? (isValidPassword = input === password); 
+
+// if (isValidPassword) {
+//   isValidPassword = input === password;
+// }
 console.log(isValidPassword);
 
 console.log('-- end --');
--- 2-control-flow/1-isolate/2-refactoring-truthiness-operators/exercises/or-to-condition.js
+++ 2-control-flow/1-isolate/2-refactoring-truthiness-operators/exercises/or-to-condition.js
@@ -1,5 +1,3 @@
-// #todo
-
 'use strict';
 
 console.log('-- begin --');
@@ -7,11 +5,17 @@ console.log('-- begin --');
 // refactor this code to use a condition instead of ||
 
 // try different values and different types
-const firstName = _;
-const lastName = _;
+const firstName = 'Dilan';
+const lastName = 'Raison';
 console.log(firstName, lastName);
 
-const hasAnAName = firstName[0] === 'A' || lastName[0] === 'A';
+const hasAnAName = false;
+
+if (firstName[0] === 'A' || lastName[0] === 'A') {
+  hasAnAName = true;
+}
+
+// const hasAnAName = firstName[0] === 'A' || lastName[0] === 'A';
 console.log(hasAnAName);
 
 console.log('-- end --');
