9d7cf545f749ebac4ee0462dc3cd793a0f62f366
boba-milktea <catherine.idv@gmail.com>
2025-03-11 10:47:30 +0100

    add functional array methods

--- lesson-plans/functional-array-methods/3-functional-array-methods/exercises/2-use-callbacks/numbery-numberify.js
+++ lesson-plans/functional-array-methods/3-functional-array-methods/exercises/2-use-callbacks/numbery-numberify.js
@@ -5,4 +5,6 @@
  * @param {string[]} arr - the array of strings
  * @returns {number[]} an array containing numbers that aren't NaN
  */
-export const numberyNumberify = () => {};
+export const numberyNumberify = (arr=[]) => {
+  return arr.map((ele) => Number(ele)).filter((ele) => !isNaN(ele)); 
+};
